27eed5fbd60757ea6218e8872f039e9eb14ab9fa
Chris Aragon <aragonchristopherray@gmail.com>
2020-04-29 13:57:58 -0400

    first commit

---
+++ server/server.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const express = require('express')
+const morgan = require('morgan')
+
+console.log(process.env.API_TOKEN)
+
+const app = express()
+
+app.use(morgan('dev'))
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+app.use(function validateBearerToken(req, res, next) {
+  const bearerToken =req.query.testing
+  const apiToken = process.env.API_TOKEN
+
+  console.log('validate bearer token middleware')
+  
+  if(bearerToken !== apiToken){
+    return res.status(401).json({error: 'ayyy man, you aint got no key, SON!'})
+  }
+  // move to the next middleware
+  next()
+})
+
+function handleGetTypes(req, res) {
+    res.json(validTypes)
+  }
+
+app.get('/types', handleGetTypes)
+
+function handleGetPokemon(req,res){
+    res.send('Hello, Pokemon!')
+}
+
+app.get('/pokemon', handleGetPokemon)
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})

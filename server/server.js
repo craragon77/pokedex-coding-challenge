2deb5f90d427211423e18b8282445fae698e72d4
Chris Aragon <aragonchristopherray@gmail.com>
2020-04-29 17:49:56 -0400

    why is helmet still showing 'X-Powered-By'

--- server/server.js
+++ server/server.js
@@ -1,18 +1,29 @@
 require('dotenv').config();
 const express = require('express')
 const morgan = require('morgan')
+const POKEDEX = require('./pokedex.json')
+const cors = require('cors')
+const helmet = require('helmet')
+
+
 
 console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
 
 app.use(function validateBearerToken(req, res, next) {
+  const bearerToken = req.get('Authorization')
+  const apiToken = process.env.API_TOKEN
   console.log('validate bearer token middleware')
-  debugger
+  if(bearerToken !== apiToken){
+    return res.status(401).json({error: 'ayyy you fucked up, gimme the code!'})
+  }
   // move to the next middleware
   next()
 })
@@ -24,7 +35,20 @@ function handleGetTypes(req, res) {
 app.get('/types', handleGetTypes)
 
 function handleGetPokemon(req,res){
-    res.send('Hello, Pokemon!')
+  let response  = POKEDEX.pokemon
+  
+  if (req.query.name) {
+    response = response.filter(pokemon =>
+      // case insensitive searching
+      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+    )
+  }
+  if (req.query.type) {
+    response = response.filter(pokemon =>
+      pokemon.type.includes(req.query.type)
+    )
+  }
+  res.send(response)
 }
 
 app.get('/pokemon', handleGetPokemon)
